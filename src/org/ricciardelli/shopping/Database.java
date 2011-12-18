32460ebb46c7ac7415be961abd7a8f6a677a6623
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-12-18 18:50:12 -0430

    Creating tables and adding sample data.

--- src/org/ricciardelli/shopping/Database.java
+++ src/org/ricciardelli/shopping/Database.java
@@ -16,27 +16,26 @@ package org.ricciardelli.shopping;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class Database extends SQLiteOpenHelper {
+	private static String DB_NAME = "shopping.db";
+	private static int DB_VERSION = 1;
 
-	public Database(Context context, String name, CursorFactory factory,
-			int version) {
-		super(context, name, factory, version);
-		// TODO Auto-generated constructor stub
+	public Database(Context context) {
+		super(context, DB_NAME, null, DB_VERSION);
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		// TODO Auto-generated method stub
-
+		db.execSQL("CREATE TABLE lists (_id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING, description TEXT);");
+		db.execSQL("CREATE TABLE products (_id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING, description TEXT, price DECIMAL, list INTEGER NOT NULL, FOREIGN KEY (list) REFERENCES lists (_id);");
+		db.execSQL("INSERT INTO lists VALUES (null, 'Testing', 'This is a sample list.', );");
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
