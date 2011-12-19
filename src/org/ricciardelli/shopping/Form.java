da5e55a5647d8c7f735ad7e7477fe47b6b34488d
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-12-19 12:48:41 -0430

    Adding a Form Java class to open the form layout.

---
+++ src/org/ricciardelli/shopping/Form.java
@@ -0,0 +1,13 @@
+package org.ricciardelli.shopping;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Form extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.form);
+	}
+}
