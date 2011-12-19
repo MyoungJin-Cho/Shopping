bca79c4f06179137bcd73ed1d88c1333df4957eb
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-12-19 19:02:33 -0430

    Adding license and methods to inflate the list.

--- src/org/ricciardelli/shopping/Shopping.java
+++ src/org/ricciardelli/shopping/Shopping.java
@@ -1,7 +1,27 @@
+/* This file is part of Shopping.
+ * Shopping is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Shopping is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Shopping.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.ricciardelli.shopping;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class Shopping extends Activity {
 
@@ -9,5 +29,35 @@ public class Shopping extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.shopping);
+		Intent intent = getIntent();
+		setTitle(intent.getExtras().getString("name").toString());
+		inflateList(intent.getExtras().getLong("id"));
+	}
+
+	private void inflateList(long id) {
+		ListView shopping = (ListView) findViewById(R.id.shopping);
+		shopping.setAdapter(getAdapter(this, id));
+	}
+
+	private ListAdapter getAdapter(Context context, long id) {
+		return new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_multiple_choice, getProducts(
+						context, id));
+	}
+
+	private String[] getProducts(Context context, long id) {
+		Main.openDatabase(context);
+		Cursor cursor = Main.db.rawQuery("SELECT * FROM products WHERE list = "
+				+ id, null);
+		String[] products = new String[cursor.getCount()];
+		short i = 0;
+		if (cursor.moveToFirst())
+			do {
+				products[i] = cursor.getString(1);
+				i++;
+			} while (cursor.moveToNext());
+		Main.closeDatabase();
+		cursor.close();
+		return products;
 	}
 }
