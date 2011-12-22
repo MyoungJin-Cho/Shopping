2247afd73c52e3482f32a58c3fed3a6fea14b57f
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-12-22 12:25:12 -0430

    Adding getVibration() method to get the preference item vibration status and use it in the code. This method should be available through an interface.

--- src/org/ricciardelli/shopping/Preferences.java
+++ src/org/ricciardelli/shopping/Preferences.java
@@ -1,13 +1,20 @@
 package org.ricciardelli.shopping;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 public class Preferences extends PreferenceActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
 	}
 
+	public static boolean getVibration(Context context) {
+		return PreferenceManager.getDefaultSharedPreferences(context)
+				.getBoolean(context.getString(R.string.vibrate_key), true);
+	}
 }
