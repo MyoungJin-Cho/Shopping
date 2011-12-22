3fe856cebc029371c57a67e96f772ce79f458baf
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-12-22 09:45:16 -0430

    Adding Preferences class extended from PreferenceActivity.

---
+++ src/org/ricciardelli/shopping/Preferences.java
@@ -0,0 +1,13 @@
+package org.ricciardelli.shopping;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Preferences extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+}
