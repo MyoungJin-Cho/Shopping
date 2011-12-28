4f68bd3ca5f9105341f1b8de75c02d3a3b08951b
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-12-28 14:15:53 -0430

    Removing the interface and creating a new class that extends from Activity.

--- src/org/ricciardelli/shopping/CRUD.java
+++ src/org/ricciardelli/shopping/CRUD.java
@@ -14,19 +14,125 @@
  */
 package org.ricciardelli.shopping;
 
+import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+import android.widget.Toast;
 
-public interface CRUD {
-	public static final long LISTS = 0;
-	public static final long PRODUCTS = 1;
+public class CRUD extends Activity {
+	private static final String LISTS = "lists";
+	private static final String PRODUCTS = "products";
+	private static final String EXTRA_NAME = "name";
+	private static final String EXTRA_ID = "id";
+	private static final long LISTS_KEY = 0;
+	private static final long PRODUCTS_KEY = 1;
+	private Database helper;
+	private SQLiteDatabase db;
 
-	void create(long id);
+	public static String getLists() {
+		return LISTS;
+	}
 
-	void read(long id);
+	public static String getProducts() {
+		return PRODUCTS;
+	}
 
-	void update(long id);
+	public static long getListsKey() {
+		return LISTS_KEY;
+	}
 
-	void delete(long id);
+	public static long getProductsKey() {
+		return PRODUCTS_KEY;
+	}
 
-	void notification(Context context, String text);
+	public static String getExtraName() {
+		return EXTRA_NAME;
+	}
+
+	public static String getExtraId() {
+		return EXTRA_ID;
+	}
+
+	private void openDatabase() {
+		helper = new Database(this);
+		db = helper.getWritableDatabase();
+	}
+
+	private void closeDatabase() {
+		db.close();
+		helper.close();
+	}
+
+	public String getName(String table, long id) {
+		openDatabase();
+		Cursor cursor = db.rawQuery("SELECT name FROM " + table
+				+ " WHERE _id = " + id, null);
+		cursor.moveToFirst();
+		String name = cursor.getString(0).toString();
+		cursor.close();
+		closeDatabase();
+		return name;
+	}
+
+	public String getDescription(String table, long id) {
+		openDatabase();
+		Cursor cursor = db.rawQuery("SELECT description FROM " + table
+				+ " WHERE _id = " + id, null);
+		cursor.moveToFirst();
+		String description = cursor.getString(0).toString();
+		cursor.close();
+		closeDatabase();
+		return description;
+	}
+
+	public double getPrice(long id) {
+		openDatabase();
+		Cursor cursor = db.rawQuery("SELECT price FROM " + PRODUCTS
+				+ " WHERE _id = " + id, null);
+		cursor.moveToFirst();
+		double price = cursor.getDouble(0);
+		closeDatabase();
+		return price;
+	}
+
+	public void create(String table, ContentValues values) {
+		openDatabase();
+		db.insert(table, null, values);
+		values.clear();
+		closeDatabase();
+	}
+
+	public Cursor getAllRows(String table) {
+		openDatabase();
+		Cursor cursor = db.rawQuery("SELECT * FROM " + table, null);
+		// closeDatabase();
+		return cursor;
+	}
+
+	public void update(String table, ContentValues values, long id) {
+		openDatabase();
+		db.update(table, values, BaseColumns._ID, null);
+		closeDatabase();
+	}
+
+	public void delete(String table, long id) {
+		openDatabase();
+		db.execSQL("DELETE FROM " + table + " WHERE _id = " + id);
+		closeDatabase();
+	}
+
+	public void notification(Context context, String text) {
+		Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+	}
+
+	public void showActivity(Context context, Class<?> c, String name, long id) {
+		Intent intent = new Intent(context, c);
+		intent.putExtra(EXTRA_NAME, name);
+		intent.putExtra(EXTRA_ID, id);
+		startActivity(intent);
+	}
 }
