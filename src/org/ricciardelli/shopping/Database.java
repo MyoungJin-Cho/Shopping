0485517c37807720befb331ed6197dba661b59c1
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-12-31 15:32:08 -0430

    Changing database version.

--- src/org/ricciardelli/shopping/Database.java
+++ src/org/ricciardelli/shopping/Database.java
@@ -20,7 +20,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class Database extends SQLiteOpenHelper {
 	private static String DB_NAME = "shopping.db";
-	private static int DB_VERSION = 48;
+	private static int DB_VERSION = 50;
 
 	public Database(Context context) {
 		super(context, DB_NAME, null, DB_VERSION);
